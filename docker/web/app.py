0c761a1a60c7dadd71a6962c023a269c546cce1c
Dave Lawson <unbrokenrabbit@gmail.com>
2018-06-23 22:25:27 -0500

    initial web app files

---
+++ docker/web/app.py
@@ -0,0 +1,62 @@
+import redis
+import sys
+import time
+from flask import Flask
+from flask import render_template
+from flask import request
+from werkzeug import secure_filename
+
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route( '/' )
+def home():
+    version = str( sys.version_info[ 0 ] ) + '.' + str( sys.version_info[ 1 ] )
+    message = 'python version: ' +  version
+
+    user = { 'username': 'Darve' }
+
+    return render_template( 'home.html', user=user, title='Moneys' )
+
+@app.route( '/import' )
+def importThings():
+    return render_template( 'import.html' )
+
+@app.route( '/import-transactions' )
+def importTransactions():
+    return render_template( 'import_transactions.html' )
+
+@app.route( '/importTransactionsCsv', methods=['GET', 'POST'] )
+def importTransactionsCsv():
+   if request.method == 'POST':
+      transactionsFile = request.files[ 'file' ]
+      secureFilename = secure_filename( transactionsFile.filename )
+      transactionsFile.save( secureFilename )
+
+      content = 'file:'
+      realFile = open( secureFilename, 'r' )
+      for line in realFile:
+         content += line
+
+      return content
+
+#@app.route('/hello')
+#def hello():
+#    count = get_hit_count()
+#    return 'Hello World! I have been seen {} times.\n'.format(count)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
